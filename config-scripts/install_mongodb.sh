07ebcbff7e29710eb966e8ddf4b77b10311826ce
Aleksandr Beresnev <aleks.beresnev@gmail.com>
2021-06-28 23:25:17 +0300

    перенос с рабочего компа на домашний

--- config-scripts/install_mongodb.sh
+++ config-scripts/install_mongodb.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
-echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list
-wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add -
-apt-get install -y apt-transport-https ca-certificates
+#echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list
+#wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add -
+#apt-get install -y apt-transport-https ca-certificates
+#apt-get --assume-yes update
+#apt-get --assume-yes install mongodb-org
+#systemctl start mongod
+#systemctl enable mongod
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/4.4 multiverse" > /etc/apt/sources.list.d/mongodb-org-4.4.list
+echo "deb http://repo.pritunl.com/stable/apt focal main" > /etc/apt/sources.list.d/pritunl.list
+apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv 9DA31620334BD75D9DCB49F368818C72E52529D4
+apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv 7568D9BB55FF9E5287D586017AE645C0CF8E292A
 apt-get --assume-yes update
+apt-get --assume-yes upgrade
 apt-get --assume-yes install mongodb-org
 systemctl start mongod
 systemctl enable mongod
